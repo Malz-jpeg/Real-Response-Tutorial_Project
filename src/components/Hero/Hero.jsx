076806941d190eb6811811876e8376fd48078b49
Malz-jpeg <m.shitu@hotmail.co.uk>
2024-11-09 17:41:10 +0000

    Fixed Hero.jsx error

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,19 +1,35 @@
-import React from 'react'
-import "./Hero.css"
+import React from 'react';
+import './Hero.css';
+
 const Hero = () => {
   return (
     <section className="hero-wrapper">
-        <div className="paddings innerWidth flexCenter hero-container">
+      <div className="paddings innerWidth flexCenter hero-container">
         
-        <div className="hero-left">
-            left section
+        {/* Left section */}
+        <div className="flexColStart hero-left">
+          <div className="hero-title">
+            <div className="orange-circle"/>
+            <h1>
+              Discover<br/> Most Suitable<br/> Property
+            </h1>
+          </div>
+
+          <div className="flexColStart hero-des">
+            <span>Find a variety of properties that suit you very easily</span>
+            <span>Forget all difficulties in finding a residence for you</span>
+          </div>
+
+          <div className="search-bar">
+            Search Bar
+          </div>
         </div>
 
-        <div className="flexcenter hero-right">
-            <div className="image-container">
-                <img src="./hero-image.png" alt="" />
-            </div>
-
+        {/* Right section */}
+        <div className="flexCenter hero-right">
+          <div className="image-container">
+            <img src="./hero-image.png" alt="Property" />
+          </div>
         </div>
 
         </div>
