b81c22ecfde5fb3f2e0f1b20ebd4ff7d3127cd4e
Malz-jpeg <m.shitu@hotmail.co.uk>
2024-12-06 01:06:27 +0000

    Fixed syntax error

--- src/components/Residencies/Residencies.jsx
+++ src/components/Residencies/Residencies.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
-import {Swiper, SwipeSlide,useSwiper} from 'swiper/react'
-import "swiper/css"
-import './Residencies.css'
-import data from '../../utils/slider.json'
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import './Residencies.css';
+import data from '../../utils/slider.json';
+
 const Residencies = () => {
   return (
     <section className="r-wrapper">
-        <div className="paddings innerWidth r-container">
-            
+      <div className="paddings innerWidth r-container">
+        <div className="r-head flexColStart">
+          <span className="orangeText">Best Choices</span>
+          <span className="primaryText">Popular Residencies</span>
         </div>
-    </section>
-  )
-}
 
-export default Residencies
+        <Swiper>
+          {data.map((card, i) => (
+            <SwiperSlide key={i}>
+              <div className="r-card">
+                <img src={card.image} alt="home" />
+                <span className="secondaryText r-price">{card.price}</span>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
+};
+
+export default Residencies;
