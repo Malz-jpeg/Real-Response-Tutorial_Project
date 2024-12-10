b5a2cd4636a8be818a57b7d1ac1cb8327c689377
Malz-jpeg <m.shitu@hotmail.co.uk>
2024-12-10 22:08:26 +0000

    Residencies- image slide

--- src/components/Residencies/Residencies.jsx
+++ src/components/Residencies/Residencies.jsx
@@ -16,9 +16,17 @@ const Residencies = () => {
         <Swiper>
           {data.map((card, i) => (
             <SwiperSlide key={i}>
-              <div className="r-card">
+              <div className="flexColStart r-card">
                 <img src={card.image} alt="home" />
                 <span className="secondaryText r-price">{card.price}</span>
+
+                <span className="secondaryText r-price">
+                  <span style={{color:"orange"}}>$</span>
+                  <span>{card.price}</span>
+                </span>
+
+                <span className="primaryText">{card.name}</span>
+                <span className="secondaryText">{card.detail}</span>
               </div>
             </SwiperSlide>
           ))}
