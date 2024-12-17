342f5e32f042c6dcd92d022369972205c16e91db
Malz-jpeg <m.shitu@hotmail.co.uk>
2024-12-17 16:34:04 +0000

    Sliders Button

--- src/components/Residencies/Residencies.jsx
+++ src/components/Residencies/Residencies.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import './Residencies.css';
-import data from '../../utils/slider.json';
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "./Residencies.css";
+import data from "../../utils/slider.json";
+import { sliderSettings } from "../../utils/common";
 
 const Residencies = () => {
+  const [swiperInstance, setSwiperInstance] = useState(null); // Track Swiper instance
+
   return (
     <section className="r-wrapper">
       <div className="paddings innerWidth r-container">
@@ -13,15 +16,16 @@ const Residencies = () => {
           <span className="primaryText">Popular Residencies</span>
         </div>
 
-        <Swiper>
-          {
-          data.map((card, i) => (
+        {/* Attach Swiper instance */}
+        <Swiper {...sliderSettings} onSwiper={(swiper) => setSwiperInstance(swiper)}>
+          <SliderButtons swiper={swiperInstance} /> {/* Pass Swiper instance */}
+          {data.map((card, i) => (
             <SwiperSlide key={i}>
               <div className="flexColStart r-card">
                 <img src={card.image} alt="home" />
 
                 <span className="secondaryText r-price">
-                  <span style={{color:"orange"}}>$</span>
+                  <span style={{ color: "orange" }}>$</span>
                   <span>{card.price}</span>
                 </span>
 
@@ -33,6 +37,15 @@ const Residencies = () => {
         </Swiper>
       </div>
     </section>
+  );
+};
+
+const SliderButtons = ({ swiper }) => {
+  return (
+    <div className="r-buttons">
+      <button onClick={() => swiper && swiper.slidePrev()}>&lt;</button> 
+      <button onClick={() => swiper && swiper.slideNext()}>&gt;</button> 
+    </div>
   );
 };
 
