12f5da73b8e3ee75082b359cf9e0cbccb3aabe72
Malz-jpeg <m.shitu@hotmail.co.uk>
2024-12-18 17:22:55 +0000

    Value-section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero/Hero';
 import './App.css'
 import Companies from './components/Hero/Companies/Companies';
 import Residencies from './components/Residencies/Residencies';
+import Value from './components/Value/Value';
 function App() {
   return (
     <div className="App">
@@ -12,7 +13,8 @@ function App() {
       <Hero/>
       </div>
       <Companies/>
-      <Residencies/>      
+      <Residencies/>
+      <Value/>
     </div>
   );
 }
---
+++ src/components/Value/Value.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+    AccordionItemState
+} from 'react-accessible-accordion';
+import "react-accessible-accordion/dist/fancy-example.css";
+import { MdOutlineArrowDropDown } from 'react-icons/md';
+import './Value.css';
+const Value = () => {
+  return (
+    <section className="v-wrapper">
+        <div className="paddings innerWidth FlexCenter v-container">
+            {/* left side */}
+            <div className="v-left">
+                <div className="image-container">
+                    <img src="./value.png" alt="" />
+                </div>
+            </div>
+            {/* right side */}
+            <div className="flexColStart v-right">
+                <span className="orangeText">Our Value</span>
+                <span className="primaryText">Value We Give to You</span>
+                <span className="secondaryText">We're always ready to help by providing the best services for you.
+                    <br/>
+                    We believe a good plave to live can make your life better
+                </span>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Value
